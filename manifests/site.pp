c6063dc40df91027c9c4fc40e55f73e17a86827e
Theo Chatzimichos <tampakrap@gentoo.org>
2013-09-29 00:36:37 +0200

    Revert "Remove the puppetmaster configuration, will add it later"
    This reverts commit 774ec0340dad85ca21337b35aed0608a5a0c59ff.
    My forked puppet-puppet module has gentoo support now

--- manifests/site.pp
+++ manifests/site.pp
@@ -11,4 +11,18 @@ node 'puppet-test1.dev.gentoo.org' {
   eselect { 'ruby':
     set => 'ruby19',
   }
+  class { 'puppet::server':
+    servertype => 'unicorn',
+    monitor_server => true,
+    backup_server => true,
+    manifest => '$confdir/environments/$environment/manifests/site.pp',
+    modulepath => '$confdir/environments/$environment/modules:$confdir/environments/$environment/modules-base:$confdir/environments/$environment/modules-services:$confdir/environments/$environment/modules-roles',
+    ca => true,
+    keywords => '~amd64',
+    ensure => 'latest',
+    use => ['augeas', 'diff', '-doc', '-emacs', 'ldap', '-minimal', 'rrdtool', 'shadow', 'sqlite3', 'vim-syntax', '-xemacs'],
+  }
+  class { 'puppet::agent':
+    server => 'puppet-test1.dev.gentoo.org',
+  }
 }
