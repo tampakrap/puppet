fb792f997195de1813d1aecba5145683c6f22265
Theo Chatzimichos <tampakrap@gentoo.org>
2013-09-29 21:03:08 +0200

    move r10k::configfile value in hiera

--- modules-services/infrastructure/manifests/puppetmaster/r10k.pp
+++ modules-services/infrastructure/manifests/puppetmaster/r10k.pp
@@ -1,9 +1,6 @@
 class infrastructure::puppetmaster::r10k {
 
-  class { '::r10k':
-    configfile => 'puppet:///modules/infrastructure/etc/r10k.yaml'
-  }
-
+  include r10k
   include r10k::cron
 
 }
