0e11ce31b312797c6def5104f8f7f624e4e4b725
Theo Chatzimichos <tampakrap@gentoo.org>
2013-09-30 22:37:06 +0200

    change the order of setting up master services

--- modules-profiles/service/manifests/puppet/master.pp
+++ modules-profiles/service/manifests/puppet/master.pp
@@ -1,13 +1,13 @@
 class service::puppet::master {
 
+  include service::puppet::master::r10k
+  include service::puppet::master::hiera
+
   $modulepath = hiera_array('puppet_modulepath')
 
   class { '::puppet::server':
     modulepath => inline_template("<%= modulepath.join(':') %>"),
   }
-
-  include service::puppet::master::hiera
-  include service::puppet::master::r10k
 
   file { '/etc/puppet/auth.conf':
     source => 'puppet:///modules/infrastructure/etc/puppet/auth.conf',
