c2feb1cc48b40c9bb866e43e6f969fa76e196e1d
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-02 12:09:25 +0200

    remove empty dirs (again)

--- modules-profiles/service/manifests/puppet/master.pp
+++ modules-profiles/service/manifests/puppet/master.pp
@@ -15,14 +15,14 @@ class service::puppet::master {
 
   # Remove empty directories
 
-  #file { '/etc/puppet/manifests':
-  #  ensure => absent,
-  #  force  => true
-  #}
+  file { '/etc/puppet/manifests':
+    ensure => absent,
+    force  => true
+  }
 
-  #file { '/etc/puppet/modules':
-  #  ensure => absent,
-  #  force  => true
-  #}
+  file { '/etc/puppet/modules':
+    ensure => absent,
+    force  => true
+  }
 
 }
