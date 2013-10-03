9a644114d36249fce79bfc9590613b726fabeded
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-03 20:58:58 +0200

    modulepath array is now supported in puppet-puppet

--- modules-profiles/service/manifests/puppet/master.pp
+++ modules-profiles/service/manifests/puppet/master.pp
@@ -3,11 +3,7 @@ class service::puppet::master {
   #include service::puppet::master::r10k
   include service::puppet::master::hiera
 
-  $modulepath = hiera_array('puppet_modulepath')
-
-  class { '::puppet::server':
-    modulepath => inline_template("<%= modulepath.join(':') %>"),
-  }
+  include ::puppet::server
 
   file { '/etc/puppet/auth.conf':
     source => 'puppet:///modules/service/etc/puppet/auth.conf',
