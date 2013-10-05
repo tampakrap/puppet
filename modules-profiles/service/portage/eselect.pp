a77b830ad6ce1aa4fd1c52b94d7c4a7f2b02011e
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-05 13:34:53 +0200

    minor fix

--- modules-profiles/service/portage/eselect.pp
+++ modules-profiles/service/portage/eselect.pp
@@ -1,9 +1,9 @@
 class service::portage::eselect (
   $ruby
-  $python:python2
+  $python2
 ) {
 
   eselect { 'ruby': set => $ruby }
-  eselect { 'python:python2': set => $python:python2 }
+  eselect { 'python:python2': set => $python2 }
 
 }
