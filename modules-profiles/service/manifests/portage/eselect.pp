eb475e50fc496b7d071981fcba7fa324e072025b
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-07 01:58:41 +0200

    more python version selections

--- modules-profiles/service/manifests/portage/eselect.pp
+++ modules-profiles/service/manifests/portage/eselect.pp
@@ -1,11 +1,15 @@
 class service::portage::eselect (
   $ruby,
+  $python,
   $python2,
+  $python3,
   $gcc,
 ) {
 
   eselect { 'ruby': set => $ruby }
+  eselect { 'python': set => $python }
   eselect { 'python::python2': set => $python2 }
+  eselect { 'python::python3': set => $python3 }
   eselect { 'gcc-config': set => $gcc }
 
 }
