6eb19fbeece94c6e3625c0f77b298314836d5998
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-07 01:35:49 +0200

    fix the hiera_array, so that use flags and features in make.conf are flattened

--- modules-profiles/service/manifests/portage/make_conf.pp
+++ modules-profiles/service/manifests/portage/make_conf.pp
@@ -14,8 +14,8 @@ class service::portage::make_conf (
 
   portage::makeconf { 'cflags': content => $cflags }
   portage::makeconf { 'makeopts': content => "-j$jobs" }
-  portage::makeconf { 'use': content => $use }
-  portage::makeconf { 'features': content => $features }
+  portage::makeconf { 'use': content => hiera_array(service::portage::make_conf::use) }
+  portage::makeconf { 'features': content => hiera_array(service::portage::mak_conf::features) }
   portage::makeconf { 'ruby_targets': content => $ruby_targets }
   portage::makeconf { 'python_targets': content => $python_targets }
 
