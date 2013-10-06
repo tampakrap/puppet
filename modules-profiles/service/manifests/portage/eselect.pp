0148ace4897983f9c290c39d8e672bfbb0c0c007
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-07 01:57:06 +0200

    Select gcc-config version

--- modules-profiles/service/manifests/portage/eselect.pp
+++ modules-profiles/service/manifests/portage/eselect.pp
@@ -1,9 +1,11 @@
 class service::portage::eselect (
   $ruby,
   $python2,
+  $gcc,
 ) {
 
   eselect { 'ruby': set => $ruby }
   eselect { 'python::python2': set => $python2 }
+  eselect { 'gcc-config': set => $gcc }
 
 }
