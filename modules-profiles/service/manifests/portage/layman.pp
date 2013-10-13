9f092397cd1298ec0f260b951def240bed0ff623
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-14 00:56:50 +0200

    check if the portage::layman class is already defined

--- modules-profiles/service/manifests/portage/layman.pp
+++ modules-profiles/service/manifests/portage/layman.pp
@@ -1,8 +1,10 @@
-class service::portage::layman {
-  portage::package { 'app-portage/layman':
-    ensure => 'latest',
-    before => Portage::Makeconf['source /var/lib/layman/make.conf'],
-  }
+if ! defined(Class['Service::Portage::Layman']) {
+  class service::portage::layman {
+    portage::package { 'app-portage/layman':
+      ensure => 'latest',
+      before => Portage::Makeconf['source /var/lib/layman/make.conf'],
+    }
 
-  portage::makeconf { 'source /var/lib/layman/make.conf': }
+    portage::makeconf { 'source /var/lib/layman/make.conf': }
+  }
 }
