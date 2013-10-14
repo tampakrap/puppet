fa4c1a1bb56e161ef5ee9d5e0f259dbf94b024d0
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-14 23:37:19 +0200

    rename the useflag_group_$use facter files

--- modules-profiles/service/manifests/portage/make_conf/useflag_group.pp
+++ modules-profiles/service/manifests/portage/make_conf/useflag_group.pp
@@ -1,5 +1,5 @@
 define service::portage::make_conf::useflag_group {
-  file { "/etc/facter/facts.d/${name}.txt":
+  file { "/etc/facter/facts.d/useflag_group_${name}.txt":
     content => "useflag_group_${name}=true",
     require => File['/etc/facter/facts.d'],
   }
