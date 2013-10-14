bd7cefc806ad3e9aef399386e931204a24650c2b
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-14 22:54:03 +0200

    fix the deps again, perform layman/webap-config operations after the facts are deployed

--- modules-profiles/webservice/manifests/okupy.pp
+++ modules-profiles/webservice/manifests/okupy.pp
@@ -14,33 +14,26 @@ class webservice::okupy (
     service::portage::make_conf::useflag_group { 'apache': }
   }
 
-  layman { 'okupy':
-    require => [
-      Service::Portage::Tool['layman'],
-      Package['app-portage/layman'],
-    ],
-  }
+  if $::gentoo_tool_layman and $::gentoo_tool_webapp_config {
+    layman { 'okupy':
+      require => Package['app-portage/layman'],
+    }
 
-  portage::package { 'www-apps/okupy':
-    before   => Webapp[$domain],
-    require  => [
-      Layman['okupy'],
-      Service::Portage::Make_conf::Useflag_group['apache'],
-    ],
-    keywords => '**',
-    ensure   => '9999',
-    target   => 'okupy',
-  }
+    portage::package { 'www-apps/okupy':
+      before   => Webapp[$domain],
+      require  => Layman['okupy'],
+      keywords => '**',
+      ensure   => '9999',
+      target   => 'okupy',
+    }
 
-  webapp { "${domain}":
-    appname    => 'okupy',
-    appversion => '9999',
-    server     => 'apache',
-    user       => 'apache',
-    group      => 'apache',
-    require    => [
-      Service::Portage::Tool['webapp_config'],
-      Package['app-admin/webapp-config'],
-    ],
+    webapp { "${domain}":
+      appname    => 'okupy',
+      appversion => '9999',
+      server     => 'apache',
+      user       => 'apache',
+      group      => 'apache',
+      require    => Package['app-admin/webapp-config'],
+    }
   }
 }
