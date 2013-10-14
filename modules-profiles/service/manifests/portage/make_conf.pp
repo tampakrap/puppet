cb234b639426752b103c9207bf281f2d18ac473d
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-14 23:57:17 +0200

    add PORTDIR in make.conf

--- modules-profiles/service/manifests/portage/make_conf.pp
+++ modules-profiles/service/manifests/portage/make_conf.pp
@@ -18,5 +18,6 @@ class service::portage::make_conf (
   portage::makeconf { 'features': content => hiera_array(service::portage::make_conf::features) }
   portage::makeconf { 'ruby_targets': content => $ruby_targets }
   portage::makeconf { 'python_targets': content => $python_targets }
+  portage::makeconf { 'portdir': content => '/usr/portage' }
 
 }
