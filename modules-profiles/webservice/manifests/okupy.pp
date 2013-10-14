17c7457914493a16ee9bfd8264feb05289bdec68
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-14 23:53:26 +0200

    comment out the installation of okupy because it needs db first

--- modules-profiles/webservice/manifests/okupy.pp
+++ modules-profiles/webservice/manifests/okupy.pp
@@ -19,21 +19,21 @@ class webservice::okupy (
       require => Package['app-portage/layman'],
     }
 
-    portage::package { 'www-apps/okupy':
-      before   => Webapp[$domain],
-      require  => Layman['okupy'],
-      keywords => '**',
-      ensure   => '9999',
-      target   => 'okupy',
-    }
+    #portage::package { 'www-apps/okupy':
+    #  before   => Webapp[$domain],
+    #  require  => Layman['okupy'],
+    #  keywords => '**',
+    #  ensure   => '9999',
+    #  target   => 'okupy',
+    #}
 
-    webapp { "${domain}":
-      appname    => 'okupy',
-      appversion => '9999',
-      server     => 'apache',
-      user       => 'apache',
-      group      => 'apache',
-      require    => Package['app-admin/webapp-config'],
-    }
+    #webapp { "${domain}":
+    #  appname    => 'okupy',
+    #  appversion => '9999',
+    #  server     => 'apache',
+    #  user       => 'apache',
+    #  group      => 'apache',
+    #  require    => Package['app-admin/webapp-config'],
+    #}
   }
 }
