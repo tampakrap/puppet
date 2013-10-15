59fc799259e9c85faeed78437cc30e2fcff5b1fc
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-15 11:13:18 +0200

    install mirrorselect in everlast for testing

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -10,6 +10,12 @@ node 'evidence.pebkac.gr' {
 node 'everlast.pebkac.gr' {
   include role::generic
   include webpage::identity
+
+  portage::package { 'app-portage/mirrorselect':
+    ensure   => '9999',
+    target   => 'mirrorselect',
+    keywords => '**',
+  }
 }
 
 node 'raekwon.pebkac.gr' {
