53e3f464f4217f0d73661772c23d204749338a6f
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-17 23:29:11 +0200

    actually make it show multiple times

--- modules-profiles/service/manifests/ssh.pp
+++ modules-profiles/service/manifests/ssh.pp
@@ -2,10 +2,10 @@ class service::ssh {
   include ::ssh
 
   ssh::config::server {
-    'Port':
-      before  => Ssh::Config::Server['ListenAddress'],
-      content => '22';
-    'ListenAddress': content => ['178.63.20.213', '2a01:4f8:141:43c1::213'];
+    'Port': content => '22';
+    'ListenAddress':
+      content => ['178.63.20.213', '2a01:4f8:141:43c1::213'],
+      multiple => true;
     'UsePAM': content => 'yes';
     #'HostKey': content => '/etc/ssh/ssh_host_rsa_key';
     #'HostKey': content => '/etc/ssh/ssh_host_dsa_key';
