e5e7340ad7389f52c9f83925a17ab614d2bf216e
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-17 22:53:53 +0200

    add more ssh items

--- modules-profiles/service/manifests/ssh.pp
+++ modules-profiles/service/manifests/ssh.pp
@@ -3,7 +3,14 @@ class service::ssh {
 
   ssh::config::server {
     'Port': content => '22';
+    'ListenAddress': content => '178.63.20.213';
+    #'ListenAddress': content => '2a01:4f8:141:43c1::213';
     'UsePAM': content => 'yes';
+    #'HostKey': content => '/etc/ssh/ssh_host_rsa_key';
+    #'HostKey': content => '/etc/ssh/ssh_host_dsa_key';
+    #'HostKey': content => '/etc/ssh/ssh_host_ecdsa_key';
+    'Subsystem': content => 'sftp /usr/lib64/misc/sftp-server';
+    'AllowUsers': content => ['tampakrap', 'tomkap'];
   }
 
 }
