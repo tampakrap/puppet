424de177d88169a54fd20e1c9fea648e4de9c577
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-17 23:42:46 +0200

    pass order, add more values for sshd_config

--- modules-profiles/service/manifests/ssh.pp
+++ modules-profiles/service/manifests/ssh.pp
@@ -2,14 +2,20 @@ class service::ssh {
   include ::ssh
 
   ssh::config::server {
-    'Port': content => '22';
+    'Port':
+      content => '22',
+      order   => 01;
     'ListenAddress':
       content => ['178.63.20.213', '2a01:4f8:141:43c1::213'],
       multiple => true;
     'UsePAM': content => 'yes';
-    #'HostKey': content => '/etc/ssh/ssh_host_rsa_key';
-    #'HostKey': content => '/etc/ssh/ssh_host_dsa_key';
-    #'HostKey': content => '/etc/ssh/ssh_host_ecdsa_key';
+    'HostKey':
+      multiple => true,
+      content  => [
+        '/etc/ssh/ssh_host_rsa_key',
+        '/etc/ssh/ssh_host_dsa_key',
+        '/etc/ssh/ssh_host_ecdsa_key'
+      ];
     'Subsystem': content => 'sftp /usr/lib64/misc/sftp-server';
     'AllowUsers': content => ['tampakrap', 'tomkap'];
   }
