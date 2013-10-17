e5a1ad4f9e45c2d397043c6ebafaa4de06f4832a
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-17 23:16:47 +0200

    multi-valued listenaddress

--- modules-profiles/service/manifests/ssh.pp
+++ modules-profiles/service/manifests/ssh.pp
@@ -5,8 +5,7 @@ class service::ssh {
     'Port':
       before  => Ssh::Config::Server['ListenAddress'],
       content => '22';
-    'ListenAddress': content => '178.63.20.213';
-    #'ListenAddress': content => '2a01:4f8:141:43c1::213';
+    'ListenAddress': content => ['178.63.20.213', '2a01:4f8:141:43c1::213'];
     'UsePAM': content => 'yes';
     #'HostKey': content => '/etc/ssh/ssh_host_rsa_key';
     #'HostKey': content => '/etc/ssh/ssh_host_dsa_key';
