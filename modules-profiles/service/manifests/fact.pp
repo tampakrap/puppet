9394804af1cdbdc8e08930590fb6c82ff5d56fed
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-19 00:46:41 +0200

    fix service::fact define, no need for $variable argument, we have $name

--- modules-profiles/service/manifests/fact.pp
+++ modules-profiles/service/manifests/fact.pp
@@ -1,12 +1,11 @@
 define service::fact (
   $ensure   = 'present',
-  $variable = '',
   $value    = '',
 ) {
 
   file { "/etc/facter/facts.d/${name}.txt":
     ensure  => $ensure,
-    content => "${variable}=${value}",
+    content => "${name}=${value}",
     require => File['/etc/facter/facts.d'],
   }
 
