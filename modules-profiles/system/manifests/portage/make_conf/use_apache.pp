9a811177b06d33a7ddaae7abea2fbc6e33530111
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-19 12:32:17 +0200

    typo

--- modules-profiles/system/manifests/portage/make_conf/use_apache.pp
+++ modules-profiles/system/manifests/portage/make_conf/use_apache.pp
@@ -1,4 +1,4 @@
-class service::portage::make_conf::use_apache {
+class system::portage::make_conf::use_apache {
 
   if ! defined(Service::Fact['useflag_group_apache']) {
     service::fact { 'useflag_group_apache': value => true }
