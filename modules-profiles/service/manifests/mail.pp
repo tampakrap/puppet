b552b8902e28330a4d0c4991264a5aada27ac6b6
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-19 20:24:48 +0200

    de-hardcode values from service::mail, move them to hieradata

--- modules-profiles/service/manifests/mail.pp
+++ modules-profiles/service/manifests/mail.pp
@@ -1,29 +1,47 @@
-class service::mail {
+class service::mail (
+  $queue_directory   = '/var/spool/postfix',
+  $command_directory = '/usr/sbin',
+  $daemon_directory  = '/usr/libexec/postfix',
+  $data_directory    = '/var/lib/postfix',
+  $mail_owner        = 'postfix',
+  $mydomain          = undef,
+  $myorigin          = '$mydomain',
+  $mydestination     = '$myhostname, localhost.$mydomain, localhost',
+  $unkown_local_recipient_reject_code = '550',
+  $alias_maps        = 'hash:/etc/aliases',
+  $alias_database    = 'hash:/etc/aliases',
+  $home_mailbox      = '.maildir/',
+  $debug_peer_level  = '2',
+  $sendmail_path     = '/usr/sbin/sendmail',
+  $newaliases_path   = '/usr/bin/newaliases',
+  $mailq_path        = '/usr/bin/mailq',
+  $setgid_group      = 'postdrop',
+  $sample_directory  = '/etc/postfix',
+  $default_filter_nexthop = '$myhostname',
+
+) {
 
   include postfix
   postfix::config {
-    'queue_directory': value => '/var/spool/postfix';
-    'command_directory': value => '/usr/sbin';
-    'daemon_directory': value => '/usr/libexec/postfix';
-    'data_directory': value => '/var/lib/postfix';
-    'mail_owner': value => 'postfix';
-    'mydomain': value => 'pebkac.gr';
-    'myorigin': value => '$mydomain';
-    'mydestination': value => '$myhostname, localhost.$mydomain, localhost';
-    'unknown_local_recipient_reject_code': value => '550';
-    'smtpd_sender_restrictions': value => 'check_recipient_access pcre:/etc/postfix/blacklisted_recipient.pcre';
-    'alias_maps': value => 'hash:/etc/aliases';
-    'alias_database': value => 'hash:/etc/aliases';
-    'home_mailbox': value => '.maildir/';
-    'debug_peer_level': value => '2';
-    'sendmail_path': value => '/usr/sbin/sendmail';
-    'newaliases_path': value => '/usr/bin/newaliases';
-    'mailq_path': value => '/usr/bin/mailq';
-    'setgid_group': value => 'postdrop';
-    'sample_directory': value => '/etc/postfix';
-    'tls_random_source': value => 'dev:/dev/urandom';
-    'tls_random_exchange_name': value => '/var/lib/postfix/tls_random_exchange.tmp';
-    'default_filter_nexthop': value => '$myhostname';
+    'queue_directory': value => $queue_directory;
+    'command_directory': value => $command_directory;
+    'daemon_directory': value => $daemon_directory;
+    'data_directory': value => $data_directory;
+    'mail_owner': value => $mail_owner;
+    'mydomain': value => $mydomain;
+    'myorigin': value => $myorigin;
+    'mydestination': value => $mydestination;
+    'unknown_local_recipient_reject_code': value => $unknown_local_recipient_reject_code;
+    'alias_maps': value => $alias_maps;
+    'alias_database': value => $alias_database;
+    'home_mailbox': value => $home_mailbox;
+    'debug_peer_level': value => $debug_peer_level;
+    'sendmail_path': value => $sendmail_path;
+    'newaliases_path': value => $newaliases_path;
+    'mailq_path': value => $mailq_path;
+    'setgid_group': value => $setgid_group;
+    'sample_directory': value => $sample_directory;
+    'default_filter_nexthop': value => $default_filter_nexthop;
   }
 
 }
