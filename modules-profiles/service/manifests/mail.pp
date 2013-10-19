7bd241a28c8df130095b97a056b6896a6074dc63
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-19 20:03:11 +0200

    s/content/value

--- modules-profiles/service/manifests/mail.pp
+++ modules-profiles/service/manifests/mail.pp
@@ -1,28 +1,28 @@
 class service::mail {
 
   postfix::config {
-    'queue_directory': content => '/var/spool/postfix';
-    'command_directory': content => '/usr/sbin';
-    'daemon_directory': content => '/usr/libexec/postfix';
-    'data_directory': content => '/var/lib/postfix';
-    'mail_owner': content => 'postfix';
-    'mydomain': content => 'pebkac.gr';
-    'myorigin': content => '$mydomain';
-    'mydestination': content => '$myhostname, localhost.$mydomain, localhost';
-    'unknown_local_recipient_reject_code': content => '550';
-    'smtpd_sender_restrictions': content => 'check_recipient_access pcre:/etc/postfix/blacklisted_recipient.pcre';
-    'alias_maps': content => 'hash:/etc/aliases';
-    'alias_database': content => 'hash:/etc/aliases';
-    'home_mailbox': content => '.maildir/';
-    'debug_peer_level': content => '2';
-    'sendmail_path': content => '/usr/sbin/sendmail';
-    'newaliases_path': content => '/usr/bin/newaliases';
-    'mailq_path': content => '/usr/bin/mailq';
-    'setgid_group': content => 'postdrop';
-    'sample_directory': content => '/etc/postfix';
-    'tls_random_source': content => 'dev:/dev/urandom';
-    'tls_random_exchange_name': content => '/var/lib/postfix/tls_random_exchange.tmp';
-    'default_filter_nexthop': content => '$myhostname';
+    'queue_directory': value => '/var/spool/postfix';
+    'command_directory': value => '/usr/sbin';
+    'daemon_directory': value => '/usr/libexec/postfix';
+    'data_directory': value => '/var/lib/postfix';
+    'mail_owner': value => 'postfix';
+    'mydomain': value => 'pebkac.gr';
+    'myorigin': value => '$mydomain';
+    'mydestination': value => '$myhostname, localhost.$mydomain, localhost';
+    'unknown_local_recipient_reject_code': value => '550';
+    'smtpd_sender_restrictions': value => 'check_recipient_access pcre:/etc/postfix/blacklisted_recipient.pcre';
+    'alias_maps': value => 'hash:/etc/aliases';
+    'alias_database': value => 'hash:/etc/aliases';
+    'home_mailbox': value => '.maildir/';
+    'debug_peer_level': value => '2';
+    'sendmail_path': value => '/usr/sbin/sendmail';
+    'newaliases_path': value => '/usr/bin/newaliases';
+    'mailq_path': value => '/usr/bin/mailq';
+    'setgid_group': value => 'postdrop';
+    'sample_directory': value => '/etc/postfix';
+    'tls_random_source': value => 'dev:/dev/urandom';
+    'tls_random_exchange_name': value => '/var/lib/postfix/tls_random_exchange.tmp';
+    'default_filter_nexthop': value => '$myhostname';
   }
 
 }
