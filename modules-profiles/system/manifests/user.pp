3562b0a4ddaaf3b4cb3726d5d58bf67d10eab842
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-19 16:11:42 +0200

    typo

--- modules-profiles/system/manifests/user.pp
+++ modules-profiles/system/manifests/user.pp
@@ -2,7 +2,7 @@ define system::user ( $attrs ) {
 
   if is_hash($attrs) {
     if $attrs[uid] { $uid = $attrs[uid] }
-    if $attrs[gid] { $uid = $attrs[gid] }
+    if $attrs[gid] { $gid = $attrs[gid] }
     if $attrs[groups] { $groups = $attrs[groups] }
     if $attrs[ensure] { $ensure = $attrs[ensure] }
   } else {
