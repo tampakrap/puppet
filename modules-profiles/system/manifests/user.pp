3248796b13aa3753a04800cb63a2f29ac0fa42f2
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-19 15:24:13 +0200

    another try to install the users when $attr[uid] is not defined

--- modules-profiles/system/manifests/user.pp
+++ modules-profiles/system/manifests/user.pp
@@ -1,9 +1,13 @@
 define system::user ( $attrs ) {
 
+  if $attrs[uid] { $uid = $attrs[uid] }
+  if $attrs[gid] { $gid = $attrs[gid] }
+  if $attrs[groups] { $groups = $attrs[groups] }
+
   user { $attrs[name]:
-    uid    => $attrs[uid],
-    gid    => $attrs[gid],
-    groups => concat([$attrs[name], 'users'], $attrs[groups]),
+    uid    => $uid,
+    gid    => $gid,
+    groups => concat([$attrs[name], 'users'], $groups),
     ensure => $attrs[ensure],
   }
 
