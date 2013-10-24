6f50f62d4a5508ec79a3824eb24f2c70b4409150
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-24 23:26:37 +0200

    Install more portage utils

--- modules-profiles/system/manifests/portage.pp
+++ modules-profiles/system/manifests/portage.pp
@@ -2,4 +2,13 @@ class system::portage {
   include ::portage
   include system::portage::make_conf
   include system::portage::eselect
+
+  package { [
+    'app-portage/gentoolkit',
+    'app-portage/gentoolkit-dev',
+    'app-portage/portage-utils',
+  ]:
+    ensure => present
+  }
+
 }
