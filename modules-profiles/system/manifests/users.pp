ec4dd7c2f2599621259802b505d28a6dda380f68
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-28 14:36:51 +0100

    add more users and include them in the appropriate boxes

--- modules-profiles/system/manifests/users.pp
+++ modules-profiles/system/manifests/users.pp
@@ -1,8 +1,28 @@
 class system::users (
+  $amy,
+  $atma,
   $dastergon,
+  $differentreality,
+  $etern4l,
+  $feedward,
+  $hwoarang,
+  $kokeroulis,
+  $liquidxd,
+  $lourdas_v,
+  $mgorny,
+  $miska,
+  $moloch,
+  $nikhatzi,
+  $parapente,
+  $pktoss,
+  $psomas,
+  $pvidalis,
   $skiarxon,
   $tampakrap,
   $tomkap,
+  $tzorvas,
+  $wired,
+  $yz250,
 ) {
 
   user { 'root':
@@ -20,10 +40,30 @@ class system::users (
   }
 
   system::user {
+    'amy': attrs => $amy;
+    'atma': attrs => $atma;
     'dastergon': attrs => $dastergon;
+    'differentreality': attrs => $differentreality;
+    'etern4l': attrs => $etern4l;
+    'feedward': attrs => $feedward;
+    'hwoarang': attrs => $hwoarang;
+    'kokeroulis': attrs => $kokeroulis;
+    'liquidxd': attrs => $liquidxd;
+    'lourdas_v': attrs => $lourdas_v;
+    'mgorny': attrs => $mgorny;
+    'miska': attrs => $miska;
+    'moloch': attrs => $moloch;
+    'nikhatzi': attrs => $nikhatzi;
+    'parapente': attrs => $parapente;
+    'pktoss': attrs => $pktoss;
+    'psomas': attrs => $psomas;
+    'pvidalis': attrs => $pvidalis;
     'skiarxon': attrs => $skiarxon;
     'tampakrap': attrs => $tampakrap;
     'tomkap': attrs => $tomkap;
+    'tzorvas': attrs => $tzorvas;
+    'wired': attrs => $wired;
+    'yz250': attrs => $yz250;
   }
 
 }
