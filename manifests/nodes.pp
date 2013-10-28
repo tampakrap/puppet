3c9fcc0c80315f4ee9489d7d9bb95e686f92076d
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-28 17:29:40 +0100

    Revert "notify tests, i have a suspicion mail sending is not working again"
    fixed
    This reverts commit cd51f9441f7160c43bcb092204a70a970e4bff38.

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,7 +1,6 @@
 node 'eyedea.pebkac.gr' {
   include role::generic
   include role::puppetmaster
-  notify { 'test1': }
 }
 
 node 'evidence.pebkac.gr' {
@@ -10,7 +9,6 @@ node 'evidence.pebkac.gr' {
 node 'everlast.pebkac.gr' {
   include role::generic
   include webpage::identity
-  notify { 'test2': }
 }
 
 node 'raekwon.pebkac.gr' {
