53db4dc3982d35b8fc2553860e586c556c67e36c
Theo Chatzimichos <tampakrap@gentoo.org>
2013-11-03 16:48:18 +0100

    Add apache logrotate for testing

--- modules-profiles/service/manifests/log.pp
+++ modules-profiles/service/manifests/log.pp
@@ -3,12 +3,18 @@ class service::log (
   $syslog_ng_ensure,
 ) {
 
-  include logrotate
-
   if defined(package['httpd']) {
     package { 'app-admin/cronolog': ensure => $cronolog_ensure }
   }
 
   package { 'app-admin/syslog-ng': ensure => $syslog_ng_ensure }
+
+  if defined(package['httpd']) {
+    logrotate::rule { 'apache':
+      path          => '/var/log/apache2/*.log',
+      sharedscripts => true,
+      postrotate    => '/etc/init.d/apache2 restart',
+    }
+  }
 
 }
