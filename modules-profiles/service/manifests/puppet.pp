980f7d6ececdc7539ddcbd4961d87981c9958801
Theo Chatzimichos <tampakrap@gentoo.org>
2013-11-13 22:12:03 +0100

    Move environment declaration to service::puppet::environment
    Move agent.pp to service/puppet.pp
    remove facter.pp as it was unused and the code was already present in agent.pp

--- modules-profiles/service/manifests/puppet.pp
+++ modules-profiles/service/manifests/puppet.pp
@@ -1,1 +1,16 @@
-class service::puppet {}
+class service::puppet (
+  $environment,
+) {
+
+  include ::puppet::agent
+
+  file { '/etc/facter': ensure => 'directory' }
+
+  file { '/etc/facter/facts.d':
+    ensure  => 'directory',
+    require => File['/etc/facter'],
+  }
+
+  defined_type::fact { 'environment': value => $environment }
+
+}
--- modules-profiles/service/manifests/puppet/facter.pp
+++
@@ -1,8 +0,0 @@
-class service::puppet::facter {
-  file { '/etc/facter': ensure => 'directory' }
-
-  file { '/etc/facter/facts.d':
-    ensure  => 'directory',
-    require => File['/etc/facter'],
-  }
-}
