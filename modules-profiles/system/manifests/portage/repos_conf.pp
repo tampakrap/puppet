2589ccfb09ff85cf77ee5b2beae939dd65666cb1
Theo Chatzimichos <tampakrap@gentoo.org>
2013-11-16 20:35:54 +0100

    fail, it needs to add setting everywhere

--- modules-profiles/system/manifests/portage/repos_conf.pp
+++ modules-profiles/system/manifests/portage/repos_conf.pp
@@ -8,15 +8,19 @@ class system::portage::repos_conf {
   ini_setting {
     'main-repo':
       section => 'DEFAULT',
+      setting => 'main-repo',
       value   => 'gentoo';
     'location':
       section => 'gentoo',
+      setting => 'location',
       value   => '/usr/portage';
     'sync-type':
       section => 'gentoo',
+      setting => 'sync-type',
       value   => 'rsync';
-    'sync_uri':
+    'sync-uri':
       section => 'gentoo',
+      setting => 'sync-uri',
       value   => 'rsync://rsync.gentoo.org/gentoo-portage';
   }
 
