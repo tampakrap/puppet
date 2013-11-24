8c90de82b294b3bca8b64603352ae21080363a37
Theo Chatzimichos <tampakrap@gentoo.org>
2013-11-24 15:10:02 +0100

    Add --quiet in EMERGE_DEFAULT_OPTS in make.conf

--- modules-profiles/system/manifests/portage/make_conf.pp
+++ modules-profiles/system/manifests/portage/make_conf.pp
@@ -1,5 +1,6 @@
 class system::portage::make_conf (
   $cflags,
+  $emerge_default_opts,
   $makeopts = inline_template("-j<%= @processorcount.to_i+1 %>"),
   $use,
   $features,
@@ -12,6 +13,7 @@ class system::portage::make_conf (
 
   portage::makeconf {
     'cflags': content => $cflags;
+    'emerge_default_opts': content => $emerge_default_opts;
     'makeopts': content => $makeopts;
     'use': content => hiera_array(system::portage::make_conf::use);
     'features': content => hiera_array(system::portage::make_conf::features);
