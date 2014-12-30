cfb30b54450cd0f1fa6b6434df316a2d4e313d01
Theo Chatzimichos <tampakrap@gmail.com>
2014-12-30 13:57:58 +0100

    Add default options for various providers in site.pp

---
+++ manifests/site.pp
@@ -0,0 +1,30 @@
+Package {
+  provider        => 'zypper',
+  install_options => {'--no-recommends'},
+}
+
+filebucket { 'remote':
+  server => 'odb.forkbomb.gr',
+  path   => false,
+}
+
+Service {
+  provider   => 'systemd',
+  hasrestart => true,
+  hasstatus  => true,
+}
+
+File {
+  ensure => present,
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0640',
+  backup => 'remote',
+}
+
+# Always show stdout/stderr in the reports
+Exec {
+  logoutput => true,
+  tries     => 2,
+  try_sleep => 5,
+}
