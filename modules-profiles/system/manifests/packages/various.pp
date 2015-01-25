f0acf77d02b1ea51d3639e8292eb176e1766cea7
Theo Chatzimichos <tampakrap@gmail.com>
2015-01-25 14:30:17 +0100

    add dependencies of facter

--- modules-profiles/system/manifests/packages/various.pp
+++ modules-profiles/system/manifests/packages/various.pp
@@ -1,13 +1,26 @@
 class system::packages::various (
+  $bind_libs,
+  $bind_utils,
+  $dmidecode,
   $etc_update,
   $fdupes,
+  $iproute2,
+  $libdb_4_8,
   $libgdbm4,
   $libICE6,
+  $libpci3,
+  $libpython2_7_1_0,
   $libruby2_1_2_1,
   $libSM6,
   $libXt6,
+  $libxtables10,
   $libyaml_0_2,
+  $lsb_release,
+  $make,
+  $pciutils,
+  $pciutils_ids,
   $perl,
+  $python_base,
   $ruby_common,
   $ruby2_1,
   $ruby2_1_rubygem_gem2rpm,
@@ -15,17 +28,31 @@ class system::packages::various (
   $sudo,
   $vim,
   $vim_data,
+  $virt_what,
 ) {
   package {
+    'bind-libs': ensure => $bind_libs;
+    'bind-utils': ensure => $bind_utils;
+    'dmidecode': ensure => $dmidecode;
     'etc-update': ensure => $etc_update;
     'fdupes': ensure => $fdupes;
+    'iproute2': ensure => $iproute2;
+    'libdb_4_8': ensure => $libdb_4_8;
     'libgdbm4': ensure => $libgdbm4;
     'libICE6': ensure => $libICE6;
+    'libpci3': ensure => $libpci3;
+    'libpython2_7-1_0': ensure => $libpython2_7_1_0;
     'libruby2_1-2_1': ensure => $libruby2_1_2_1;
     'libSM6': ensure => $libSM6;
     'libXt6': ensure => $libXt6;
+    'libxtables10': ensure => $libxtables10;
     'libyaml-0-2': ensure => $libyaml_0_2;
+    'lsb-release': ensure => $lsb_release;
+    'make': ensure => $make;
+    'pciutils': ensure => $pciutils;
+    'pciutils-ids': ensure => $pciutils_ids;
     'perl': ensure => $perl;
+    'python-base': ensure => $python_base;
     'ruby-common': ensure => $ruby_common;
     'ruby2.1': ensure => $ruby2_1;
     'ruby2.1-rubygem-gem2rpm': ensure => $ruby2_1_rubygem_gem2rpm;
@@ -33,5 +60,6 @@ class system::packages::various (
     'sudo': ensure => $sudo;
     'vim': ensure => $vim;
     'vim-data': ensure => $vim_data;
+    'virt-what': ensure => $virt_what;
   }
 }
